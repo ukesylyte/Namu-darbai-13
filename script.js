4179e176a749d4c7ef5c6f7c895e362fde05ae40
ukesylyte <ugnekesylyte@gmail.com>
2023-04-21 21:34:06 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+var addButton = document.getElementById("add");
+var removeFirstButton = document.getElementById("remove-first");
+var removeLastButton = document.getElementById("remove-last");
+var vardasInput = document.getElementById("vardasInput");
+var pavardeInput = document.getElementById("pavardeInput");
+var amziusInput = document.getElementById("amziusInput");
+var table = document.getElementById("table");
+
+addButton.addEventListener("click", function() {
+    var vardas = vardasInput.value;
+    var pavardė = pavardeInput.value;
+    var amžius = amziusInput.value;
+    if (vardas && pavardė && amžius) {
+        var row = table.insertRow(-1);
+        var vardasCell = row.insertCell(0);
+        var pavardėCell = row.insertCell(1);
+        var amžiusCell = row.insertCell(2);
+        vardasCell.textContent = vardas;
+        pavardėCell.textContent = pavardė;
+        amžiusCell.textContent = amžius;
+        vardasInput.value = "";
+        pavardeInput.value = "";
+        amziusInput.value = "";
+    } else {
+        alert("Laukelis tuščias :)");
+    }
+})
+
+removeFirstButton.addEventListener("click", function() {
+    if (table.rows.length > 1) {
+        table.deleteRow(1);
+    } else {
+        alert("Nebėra ką trinti");
+    }
+})
+
+removeLastButton.addEventListener("click", function() {
+    if (table.rows.length > 1) {
+        table.deleteRow(-1);
+    } else {
+        alert("Nebėra ką trinti");
+    }
+})
